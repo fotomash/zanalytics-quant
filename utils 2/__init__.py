727d46ccf8c4b776b37c916fb5979a9783e4bafa
Tom Laskowski <44295192+fotomash@users.noreply.github.com>
2025-09-09 13:27:27 +0100

    chore: remove redundant utils 2 directory

--- utils 2/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Utility helpers for the Zanalytics dashboard."""
-
-from __future__ import annotations
-
-from .enrichment import enrich_ticks, aggregate_ticks_to_bars
-
-__all__ = ["enrich_ticks", "aggregate_ticks_to_bars"]
--- utils 2/ncos_advanced_theory.py
+++
@@ -1,1 +0,0 @@
-# Advanced Theory Engine code placeholder
