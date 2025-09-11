eefebb0e232e0ade718557e9f4ded5804611a661
Tom Laskowski <44295192+fotomash@users.noreply.github.com>
2025-09-11 09:10:28 +0100

    refactor: migrate alert manager service

---
+++ services/alert_manager/__init__.py
@@ -0,0 +1,13 @@
+"""Alert manager service providing a simple dispatch API."""
+
+from .manager import Alert, AlertManager
+
+_default_manager = AlertManager()
+
+
+def send_alert(alert: Alert):
+    """Send ``alert`` using the default :class:`AlertManager` instance."""
+    return _default_manager.send_alert(alert)
+
+
+__all__ = ["Alert", "AlertManager", "send_alert"]
