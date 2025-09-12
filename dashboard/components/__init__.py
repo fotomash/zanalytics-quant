a12433802f8110859d2c0f82e77dd5cc2b7e7774
Tom Laskowski <44295192+fotomash@users.noreply.github.com>
2025-09-12 22:38:10 +0100

    x

--- dashboard/components/__init__.py
+++ dashboard/components/__init__.py
@@ -3,11 +3,7 @@
 from .chip import chip
 from .charts import create_metric_donut
 from .confluence_display import render_confluence_gates, get_confluence_weights
-from .diagnostics_panel import (
-    diagnostics_panel,
-    AGGREGATOR_URL,
-    render_diagnostics_panel,
-)
+from .diagnostics_panel import diagnostics_panel, AGGREGATOR_URL
 from .chart_builder import create_enhanced_wyckoff_chart
 from .indicator_admin import indicator_admin
 
